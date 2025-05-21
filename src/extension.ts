8f1450a269ebdf2e0e6188c5cef56d03cb9e7eb0
Benson Oyugi <bensonoyugi@protonmail.com>
2025-05-21 13:04:26 +0300

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// This method is called when the extension is activated
+// The extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	const centerEditorLayout = () => {
 		vscode.commands.executeCommand("workbench.action.centerLayout");
@@ -28,5 +27,5 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+// This method is called when the extension is deactivated
 export function deactivate() {}
